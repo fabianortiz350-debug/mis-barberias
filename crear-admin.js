c7fa3ed330dea47d1c9c3bf8cbeeef3a7dd2f462
Fabian <fabianortiz350@gmail.com>
2026-03-26 20:59:42 +0100

    Reparando servidor con bcryptjs

--- crear-admin.js
+++ crear-admin.js
@@ -3,12 +3,12 @@ const axios = require('axios');
 const crearPrimerAdmin = async () => {
     try {
         const res = await axios.post('https://mis-barberias.onrender.com/api/auth/registrar-interno', {
-            correo: "tu-correo@gmail.com", // PON TU CORREO AQUÍ
-            password: "TuPasswordSeguro123", // PON TU CONTRASEÑA AQUÍ
+            correo: "tu-correo@gmail.com", // 👈 PON TU CORREO AQUÍ
+            password: "TuPassword123",     // 👈 PON TU CONTRASEÑA AQUÍ
             rol: "SUPER",
             nombre: "Fabian Ortiz"
         });
-        console.log("✅ Usuario creado con éxito:", res.data);
+        console.log("✅ ¡SUPER ADMIN CREADO CON ÉXITO!", res.data);
     } catch (e) {
         console.error("❌ Error al crear usuario:", e.response ? e.response.data : e.message);
     }
